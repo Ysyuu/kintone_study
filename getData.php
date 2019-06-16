22d8181439f8acc3894a35fed53c9933cab0ffbe
“y_banno” <kaka9174@gmail.com>
2019-06-17 00:24:00 +0900

    勉強用：レコード情報の取得

---
+++ getData.php
@@ -0,0 +1,27 @@
+<?php
+	// 自分のkintoneの設定
+	define("API_TOKEN", "1NI3zgLP7sUbFVvCqAfuXSJJt4AAF7n6wbASaCIy"); 
+	define("SUB_DOMAIN", "banno"); 
+	define("APP_NO", "2"); 
+	//サーバ送信するHTTPヘッダを設定
+	$options = array(
+	    'http'=>array(
+	        'method'=>'GET',
+	        'header'=> "X-Cybozu-API-Token:". API_TOKEN ."\r\n"
+	    )
+	);
+	//コンテキストを生成
+    $context = stream_context_create( $options );
+    
+	// サーバに接続してデータを取得
+	$contents = file_get_contents( 'https://'. SUB_DOMAIN .'.cybozu.com/k/v1/records.json?app='. APP_NO , FALSE, $context );
+	//var_dump($http_response_header); //ヘッダ表示
+	
+	//JSON形式からArrayに変換
+    $data = json_decode($contents, true);
+    
+    //取得データを表示
+    var_dump($data);
+
+    exit;
+?>
